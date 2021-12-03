5b40d5c58106baa822c03ac9a749fa5ea3d561e0
IrvinA <arevalosirvin@gmail.com>
2021-12-03 04:11:10 -0800

    action middleware wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,21 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+
+function validateActionID(req, res, next) {
+  Actions.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        next({
+          status: 404,
+          message: 'Action not found',
+        });
+      }
+    })
+    .catch(next);
+}
+
+module.exports = {
+  validateActionID,
+};
