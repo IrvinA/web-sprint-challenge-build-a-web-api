66ab4460292e593aaf1d80114ebdac38c51476a0
IrvinA <arevalosirvin@gmail.com>
2021-12-03 03:28:21 -0800

    server.js complete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found!' });
+});
+
+// eslint-disable-next-line no-unused-vars
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
