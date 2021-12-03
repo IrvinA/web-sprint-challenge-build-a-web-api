738871da8090c634b21bba67658288a89562b8ec
IrvinA <arevalosirvin@gmail.com>
2021-12-03 03:35:22 -0800

    first project middleware complete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,21 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function validateProjectID(req, res, next) {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        next({
+          status: 404,
+          message: 'Project not found',
+        });
+      }
+    })
+    .catch(next);
+}
+
+module.exports = {
+    validateProjectID,
+}
