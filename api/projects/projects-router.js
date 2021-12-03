f88f5a26f3de800edf598626826d8d965ae42449
IrvinA <arevalosirvin@gmail.com>
2021-12-03 03:51:14 -0800

    get endpoint complete

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -10,4 +10,14 @@ const Projects = require('./projects-model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch(next);
+});
+
+
+
 module.exports = router;
