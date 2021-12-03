07ad7c9186868a59b6b59d66191905ebafd773e7
IrvinA <arevalosirvin@gmail.com>
2021-12-03 03:55:29 -0800

    post endpoint complete

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -23,4 +23,12 @@ router.get('/:id', validateProjectID, (req, res, next) => {
   res.status(200).json(req.project);
 });
 
+router.post('/', validatePost, (req, res, next) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
+
 module.exports = router;
