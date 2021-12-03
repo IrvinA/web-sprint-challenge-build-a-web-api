11da59912ae0e18abebcc1bf1b997cd60de45a8f
IrvinA <arevalosirvin@gmail.com>
2021-12-03 04:21:35 -0800

    action get endpoint complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
-const { 
-  validateActionID, 
-  validateActionPost 
+const {
+  validateActionID,
+  validateActionPost,
 } = require('./actions-middleware');
 
 const Actions = require('./actions-model');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
 
 module.exports = router;
